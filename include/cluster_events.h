a07fedc92062fa729d273e9c3ea7e486d71f79a4
Bjartur Mortensen <bjarturm+git@gmail.com>
2024-04-06 13:29:10 +0100

    Progress towards writing to file

--- include/cluster_events.h
+++ include/cluster_events.h
@@ -5,20 +5,24 @@
 #include <TFile.h>
 #include <vector>
 
-// Abstract class
 class ClusterEvents{
 private:
 	TFile &events_file;
 	// vector of Clustering?
 
 public:
-	bool RunClustering(std::vector<Clustering*> &cluster_vec);
+	// For each event performs clustering according to a list.
+	// The list of Clustering is prepared outside.
+	bool run_clustering(std::vector<Clustering*> &cluster_vec);
 
-	// Derived classes must implement this.
-	// return vector?
-	virtual bool PrepareClustering() = 0;
 
-	//
+
+	// Writes the clusters for each event to files.
+	bool write_short_format();
+
+
+	// Writes every cell and their tags to files.
+	bool write_long_format();
 
 };
 
